3fe714b68cc864287c5bbf83f7d094f47227f5fb
Hank M. Yang <chptxy@gmail.com>
2016-12-07 14:27:45 +0800

    initial commit

---
+++ src/mattsum/simple_example/core.cljs
@@ -0,0 +1,48 @@
+(ns mattsum.simple-example.core
+  (:require-macros [rum.core :refer [defc]])
+  (:require [re-natal.support :as support]
+            [rum.core :as rum]))
+
+(set! js/window.React (js/require "react"))
+(def ReactNative (js/require "react-native"))
+
+(defn create-element [rn-comp opts & children]
+      (apply js/React.createElement rn-comp (clj->js opts) children))
+
+(def app-registry (.-AppRegistry ReactNative))
+(def view (partial create-element (.-View ReactNative)))
+(def text (partial create-element (.-Text ReactNative)))
+(def image (partial create-element (.-Image ReactNative)))
+(def touchable-highlight (partial create-element (.-TouchableHighlight ReactNative)))
+
+(def logo-img (js/require "../../assets/cljs.png"))
+
+(defn alert [title]
+      (.alert (.-Alert ReactNative) title))
+
+(defonce app-state (atom {:greeting "Hello 1Clojure in iOS and Android!"}))
+
+(defc AppRoot < rum/cursored-watch [state]
+          (view {:style {:flexDirection "column" :margin 40 :alignItems "center"}}
+                (text {:style {:fontSize 30 :fontWeight "100" :marginBottom 20 :textAlign "center"}} (:greeting @state))
+                (image {:source logo-img
+                        :style  {:width 80 :height 80 :marginBottom 30}})
+                (touchable-highlight {:style   {:backgroundColor "#999" :padding 10 :borderRadius 5}
+                                      :onPress #(alert "HELLO!")}
+                                     (text {:style {:color "white" :textAlign "center" :fontWeight "bold"}} "press me"))))
+
+(defonce root-component-factory (support/make-root-component-factory))
+
+(defn mount-app [] (support/mount (AppRoot app-state)))
+
+(defn init []
+      (mount-app)
+      (.registerComponent app-registry "SimpleExampleApp" (fn [] root-component-factory)))
+
+(defn ^:export main
+  []
+  (init))
+
+(defn on-js-reload
+  []
+  (println "on-js-reload. state" ))
